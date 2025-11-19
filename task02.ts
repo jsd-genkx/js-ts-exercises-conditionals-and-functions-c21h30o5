abd386fbaa923f2e1a4288ace3970fa27c42d852
c21h30o5 <161247424+c21h30o5@users.noreply.github.com>
2025-11-19 15:51:00 +0700

    update task 01 02

--- task02.ts
+++ task02.ts
@@ -1,5 +1,28 @@
 // TODO-02: Use if/else to check if it's a number
 // (hint: typeof or isNaN)
 
-const userInput = "42";
+const userInput = ["hello", 10, null, NaN];
 // Try changing to "hello", 10, null, NaN
+
+for (let i=0; i<userInput.length; i++) {
+
+        if ( ( typeof(userInput[i])==="number" ) && !( isNaN(userInput[i]) )) {
+            console.log(`True : ${userInput[i]} is a number.`);
+        } else {
+            console.log(`False: ${userInput[i]} is a ${typeof(userInput[i])}`);
+        };
+};
+
+
+
+/*if (typeof(userInput) === "number") {
+    console.log("It's number.");
+} else {
+    console.log("NO, it's not!!!!!!!!!!!");
+};*/
+
+/*if (isNaN(userInput)) {
+    console.log("isNaN")
+} else {
+    console.log("gotchaaa");
+};*/
