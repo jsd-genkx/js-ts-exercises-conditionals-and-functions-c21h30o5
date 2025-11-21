79f92f98fa69cb582e7657b3468b6842bc19e9de
c21h30o5 <161247424+c21h30o5@users.noreply.github.com>
2025-11-21 14:09:14 +0700

    add function fizzBuzz

--- task04.ts
+++ task04.ts
@@ -8,12 +8,37 @@
 
 const num = [15, 9, 25];
 
-for (let i=0; i<num.length; i++) {
+/*for (let i=0; i<num.length; i++) {
     if (num[i] % 3 === 0 && num[i] % 5 === 0) {
         console.log("FizzBuzz");
     } else if (num[i] % 3 === 0) {
         console.log("Fizz");
     } else if (num[i] % 5 === 0) {
         console.log("Buzz");
-    };
-}
+    } else {
+        console.log("Not Divisible by 3 or 5");
+    }
+}*/
+
+const fizzBuzz = (number: number) => {
+  if (number % 3 === 0 && number % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (number % 3 === 0) {
+    console.log("Fizz");
+  } else if (number % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log("Not Divisible by 3 or 5");
+  }
+};
+
+fizzBuzz(15);
+
+const fizzBuzz2 = fizzBuzz
+fizzBuzz2(15.0);
+
+// Example test case
+fizzBuzz(15); //FizzBuzz
+fizzBuzz(9); //Fizz
+fizzBuzz(10); //Buzz
+fizzBuzz(7); //Not Divisible by 3 or 5
